ac1585ec5c7a6ba4a84a6f04b4e5484ee2c144d0
tanimul <tanimulislam.fuad@gmail.com>
2024-02-28 13:52:17 +0600

    Swipe functionalities added.

---
+++ app/src/main/java/com/tanimul/directionalswipebutton/DirectionalSwipeButton.kt
@@ -0,0 +1,125 @@
+package com.tanimul.directionalswipebutton
+
+import android.animation.ValueAnimator
+import android.annotation.SuppressLint
+import android.content.Context
+import android.util.AttributeSet
+import android.util.TypedValue
+import android.view.LayoutInflater
+import android.view.MotionEvent
+import android.view.View.OnTouchListener
+import android.view.animation.AccelerateDecelerateInterpolator
+import android.widget.*
+import androidx.annotation.Dimension
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.tanimul.directionalswipebutton.Animations.animateFadeHide
+import com.tanimul.directionalswipebutton.Animations.animateFadeShow
+import com.tanimul.directionalswipebutton.databinding.LayoutDirectionalSwipeBinding
+import com.tanimul.directionalswipebutton.extentions.convertDpToPx
+
+class DirectionalSwipeButton : ConstraintLayout {
+
+    private lateinit var binding: LayoutDirectionalSwipeBinding
+
+    constructor(context: Context) : super(context) {
+        init(context)
+    }
+
+    constructor(context: Context, attrs: AttributeSet? = null) : super(context, attrs) {
+        init(context)
+    }
+
+    constructor(
+        context: Context, attrs: AttributeSet?, defStyleAttr: Int
+    ) : super(context, attrs, defStyleAttr) {
+        init(context)
+    }
+
+    private fun init(context: Context) {
+        binding = LayoutDirectionalSwipeBinding.inflate(LayoutInflater.from(context), this, true)
+    }
+
+    override fun onFinishInflate() {
+        super.onFinishInflate()
+
+        setupTouchListener()
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    private fun setupTouchListener() {
+        setOnTouchListener(OnTouchListener { _, event ->
+            when (event.action) {
+                MotionEvent.ACTION_DOWN -> return@OnTouchListener true
+
+                MotionEvent.ACTION_MOVE -> {
+                    // Movement logic here
+                    if (event.x > binding.ivSlide.width / 2 && event.x + binding.ivSlide.width / 2 < width && (event.x < binding.ivSlide.x + binding.ivSlide.width || binding.ivSlide.x != 0f)) {
+                        // snaps the hint to user touch, only if the touch is within hint width or if it has already been displaced
+                        binding.ivSlide.x = event.x - binding.ivSlide.width / 2
+                    }
+                    if (binding.ivSlide.x + binding.ivSlide.width > width && binding.ivSlide.x + binding.ivSlide.width / 2 < width) {
+                        // allows the hint to go up to a max of btn container width
+                        binding.ivSlide.x = (width - binding.ivSlide.width).toFloat()
+                    }
+                    if (event.x < binding.ivSlide.width / 2 && binding.ivSlide.x > 0) {
+                        // allows the hint to go up to a min of btn container starting
+                        binding.ivSlide.x = 0f
+                    }
+                    return@OnTouchListener true
+                }
+
+                MotionEvent.ACTION_UP -> {
+                    //Release logic here
+                    when {
+                        binding.ivSlide.x + binding.ivSlide.width > width * 0.8f -> {
+                            // Swipe completed, Great!
+                            successfulSwipe()
+                        }
+
+                        else -> {
+                            // Swipe not completed, animate back
+                            animateBack()
+                        }
+                    }
+                    return@OnTouchListener true
+                }
+            }
+            false
+        })
+    }
+
+    private fun animateBack() {
+        ValueAnimator.ofFloat(binding.ivSlide.x, 4.convertDpToPx().toFloat()).apply {
+            interpolator = AccelerateDecelerateInterpolator()
+            addUpdateListener { binding.ivSlide.x = it.animatedValue as Float }
+            duration = 200
+            start()
+        }
+    }
+
+    private fun successfulSwipe() {
+        animateFadeHide(context, binding.layoutSlide)
+        animateFadeShow(context, binding.layoutMarked)
+
+        /*        binding.apply {
+                    layoutSlide.visibility = View.GONE
+                    layoutMarked.visibility = View.VISIBLE
+                }*/
+    }
+
+    fun setTextSize(@Dimension textSize: Int) {
+        binding.apply {
+            tvSlide.setTextSize(TypedValue.COMPLEX_UNIT_PX, textSize.toFloat())
+            tvMarked.setTextSize(TypedValue.COMPLEX_UNIT_PX, textSize.toFloat())
+        }
+    }
+
+    fun setSlideTextLabel(slideText: CharSequence) {
+        binding.tvSlide.text = slideText
+    }
+
+    fun setMarkedTextLabel(markedText: CharSequence) {
+        binding.tvMarked.text = markedText
+    }
+
+}
