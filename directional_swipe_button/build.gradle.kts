337ab177ef1b747d0eb42c3052f4825e308e0d39
tanimul <tanimulislam.fuad@gmail.com>
2024-02-29 11:48:00 +0600

    Maven-publish added.

--- directional_swipe_button/build.gradle.kts
+++ directional_swipe_button/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
+    id ("maven-publish")
 }
 
 android {
@@ -43,4 +44,16 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+}
+
+
+publishing {
+    publications {
+        create<MavenPublication>("release") {
+//            from(components["release"])
+            groupId = "com.github.tanimul"
+            artifactId = "Directional-Swipe-Button"
+            version = "1.0.0"
+        }
+    }
 }
